abd481e3e669cb8ba28739884159982dbf31d059
Bryant Moscon <bmoscon@gmail.com>
2020-12-27 13:45:10 -0500

    More modularization

--- orderbook/orderbook.c
+++ orderbook/orderbook.c
@@ -56,16 +56,6 @@ static int Orderbook_init(Orderbook *self, PyObject *args, PyObject *kwds)
 }
 
 
-static PyMemberDef Orderbook_members[] = {
-    {"bids", T_OBJECT_EX, offsetof(Orderbook, bids), READONLY, "bids"},
-    {"bid", T_OBJECT_EX, offsetof(Orderbook, bids), READONLY, "bids"},
-    {"asks", T_OBJECT_EX, offsetof(Orderbook, asks), READONLY, "asks"},
-    {"ask", T_OBJECT_EX, offsetof(Orderbook, asks), READONLY, "asks"},
-    {"max_depth", T_INT, offsetof(Orderbook, max_depth), READONLY, "Maximum book depth"},
-    {NULL}
-};
-
-
 /* Orderbook methods */
 static PyObject* Orderbook_todict(Orderbook *self, PyObject *Py_UNUSED(ignored))
 {
@@ -107,18 +97,15 @@ static PyObject* Orderbook_todict(Orderbook *self, PyObject *Py_UNUSED(ignored))
 }
 
 
-static PyMethodDef Orderbook_methods[] = {
-    {"to_dict", (PyCFunction) Orderbook_todict, METH_NOARGS, "Return a python dictionary with bids and asks"},
-    {NULL}
-};
-
-
 /* Orderbook Mapping Functions */
-Py_ssize_t Orderbook_len(Orderbook *self) {
+Py_ssize_t Orderbook_len(Orderbook *self)
+{
 	return SortedDict_len(self->bids) + SortedDict_len(self->asks);
 }
 
-PyObject *Orderbook_getitem(Orderbook *self, PyObject *key) {
+
+PyObject *Orderbook_getitem(Orderbook *self, PyObject *key)
+{
     if (!PyUnicode_Check(key)) {
         PyErr_SetString(PyExc_ValueError, "key must one of bid/ask");
         return NULL;
@@ -149,7 +136,8 @@ PyObject *Orderbook_getitem(Orderbook *self, PyObject *key) {
 }
 
 
-int Orderbook_setitem(Orderbook *self, PyObject *key, PyObject *value) {
+int Orderbook_setitem(Orderbook *self, PyObject *key, PyObject *value)
+{
     return -1;
     /*
     if (!PyDict_Check(value)) {
@@ -165,40 +153,6 @@ int Orderbook_setitem(Orderbook *self, PyObject *key, PyObject *value) {
     }
     */
 }
-
-
-/* Orderbook mapping functions */
-static PyMappingMethods Orderbook_mapping = {
-	(lenfunc)Orderbook_len,
-	(binaryfunc)Orderbook_getitem,
-    (objobjargproc)Orderbook_setitem
-};
-
-
-/* Orderbook Type Setup */
-static PyTypeObject OrderbookType = {
-    PyVarObject_HEAD_INIT(NULL, 0)
-    .tp_name = "order_book.OrderBook",
-    .tp_doc = "An Orderbook data structure",
-    .tp_basicsize = sizeof(Orderbook),
-    .tp_itemsize = 0,
-    .tp_flags = Py_TPFLAGS_DEFAULT | Py_TPFLAGS_BASETYPE,
-    .tp_new = Orderbook_new,
-    .tp_init = (initproc) Orderbook_init,
-    .tp_dealloc = (destructor) Orderbook_dealloc,
-    .tp_members = Orderbook_members,
-    .tp_methods = Orderbook_methods,
-    .tp_as_mapping = &Orderbook_mapping,
-};
-
-
-/* Module specific definitions and initilization */
-static PyModuleDef orderbookmodule = {
-    PyModuleDef_HEAD_INIT,
-    .m_name = "order_book",
-    .m_doc = "Orderbook data structure",
-    .m_size = -1,
-};
 
 
 PyMODINIT_FUNC PyInit_order_book(void)
--- orderbook/orderbook.h
+++ orderbook/orderbook.h
@@ -25,4 +25,67 @@ typedef struct {
 } Orderbook;
 
 
+static void Orderbook_dealloc(Orderbook *self);
+static PyObject *Orderbook_new(PyTypeObject *type, PyObject *args, PyObject *kwds);
+static int Orderbook_init(Orderbook *self, PyObject *args, PyObject *kwds);
+
+static PyObject* Orderbook_todict(Orderbook *self, PyObject *Py_UNUSED(ignored));
+
+Py_ssize_t Orderbook_len(Orderbook *self);
+PyObject *Orderbook_getitem(Orderbook *self, PyObject *key);
+int Orderbook_setitem(Orderbook *self, PyObject *key, PyObject *value);
+
+
+// Orderbook class members
+static PyMemberDef Orderbook_members[] = {
+    {"bids", T_OBJECT_EX, offsetof(Orderbook, bids), READONLY, "bids"},
+    {"bid", T_OBJECT_EX, offsetof(Orderbook, bids), READONLY, "bids"},
+    {"asks", T_OBJECT_EX, offsetof(Orderbook, asks), READONLY, "asks"},
+    {"ask", T_OBJECT_EX, offsetof(Orderbook, asks), READONLY, "asks"},
+    {"max_depth", T_INT, offsetof(Orderbook, max_depth), READONLY, "Maximum book depth"},
+    {NULL}
+};
+
+
+// Orderbook class methods
+static PyMethodDef Orderbook_methods[] = {
+    {"to_dict", (PyCFunction) Orderbook_todict, METH_NOARGS, "Return a python dictionary with bids and asks"},
+    {NULL}
+};
+
+
+// Orderbook mapping functions
+static PyMappingMethods Orderbook_mapping = {
+	(lenfunc)Orderbook_len,
+	(binaryfunc)Orderbook_getitem,
+    (objobjargproc)Orderbook_setitem
+};
+
+
+// Orderbook Type Setup
+static PyTypeObject OrderbookType = {
+    PyVarObject_HEAD_INIT(NULL, 0)
+    .tp_name = "order_book.OrderBook",
+    .tp_doc = "An Orderbook data structure",
+    .tp_basicsize = sizeof(Orderbook),
+    .tp_itemsize = 0,
+    .tp_flags = Py_TPFLAGS_DEFAULT | Py_TPFLAGS_BASETYPE,
+    .tp_new = Orderbook_new,
+    .tp_init = (initproc) Orderbook_init,
+    .tp_dealloc = (destructor) Orderbook_dealloc,
+    .tp_members = Orderbook_members,
+    .tp_methods = Orderbook_methods,
+    .tp_as_mapping = &Orderbook_mapping,
+};
+
+
+// Module specific definitions and initilization
+static PyModuleDef orderbookmodule = {
+    PyModuleDef_HEAD_INIT,
+    .m_name = "order_book",
+    .m_doc = "Orderbook data structure",
+    .m_size = -1,
+};
+
+
 #endif
