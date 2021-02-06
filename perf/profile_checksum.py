b280c6d9851696baf9dc449ee8ecd0ac587fdac9
Bryant Moscon <bmoscon@gmail.com>
2021-02-05 20:25:56 -0500

    Update profile_checksum.py

--- perf/profile_checksum.py
+++ perf/profile_checksum.py
@@ -1,3 +1,8 @@
+'''
+Copyright (C) 2020-2021  Bryant Moscon - bmoscon@gmail.com
+Please see the LICENSE file for the terms and conditions
+associated with this software.
+'''
 from decimal import Decimal
 import time
 import zlib
