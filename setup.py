740637218880c4cc0aab66df8085a70dd177c05b
Bryant Moscon <bmoscon@gmail.com>
2022-04-17 14:42:39 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ class Test(TestCommand):
 
 setup(
     name='order_book',
-    version='0.4.2',
+    version='0.4.3',
     author="Bryant Moscon",
     author_email="bmoscon@gmail.com",
     description="A fast orderbook implementation, in C, for Python",
