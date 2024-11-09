0c32932dd9f0f79b30897ecdcd8ea445efb4ab1d
Bryant Moscon <bmoscon@gmail.com>
2024-11-08 19:14:43 -0500

    Minor build tweaks

--- wheels.sh
+++ wheels.sh
@@ -1,2 +1,2 @@
-docker run --rm -v `pwd`:/io quay.io/pypa/manylinux2010_x86_64 /io/build-wheels.sh
+docker run --rm -v `pwd`:/io quay.io/pypa/manylinux_2_28_x86_64 /io/build-wheels.sh
 #python setup.py bdist_wheel
